0d869b8161e3f93cf85d3c1e48e16b3ec9a09144
haru0307 <3213052@stu.hus.ac.jp>
2024-01-16 23:48:31 +0900

    Add itiou

---
+++ Assets/script/haru/SoundControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource m_MyAudioSource;
+    public float vol;
+    [SerializeField]
+    private AudioClip[] clips;
+
+    void Start()
+    {
+        m_MyAudioSource = GetComponent<AudioSource>();
+        m_MyAudioSource.clip = clips[0];
+        m_MyAudioSource.Play();
+    }
+
+    private void Update()
+    {
+        Debug.Log(m_MyAudioSource.time);
+        if(!m_MyAudioSource.isPlaying&&m_MyAudioSource.time==0)
+        {
+            Debug.Log("aa");
+            m_MyAudioSource.volume = vol;
+            m_MyAudioSource.clip = clips[1];
+            m_MyAudioSource.Play();
+        }
+    }
+
+}
