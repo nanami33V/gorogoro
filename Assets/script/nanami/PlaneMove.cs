7cf35fd0ee5a651019643b743d46a2e1d4162f30
nanami33V <3213007@stu.hus.ac.jp>
2024-03-15 02:12:19 +0900

    ステージ生成を抜本的に変更

--- Assets/script/nanami/PlaneMove.cs
+++ Assets/script/nanami/PlaneMove.cs
@@ -5,74 +5,87 @@ using UnityEngine;
 
 public class PlaneMove : MonoBehaviour
 {
-    public GameObject[] Plane;
+    public GameObject[] Planes;
     public GameObject Maru;
-    private GameObject[] step = new GameObject[5];
-    private GameObject[] poststep = new GameObject[5];
-    float posX = 10;
-    float postposX = 200;
-    float posY = -2;
-    int planesize = 8;
-    bool lastplane = false;
+    public GameObject StartGround;
+
+    private int chunkSize = 10;
+    private GameObject[] ActiveChunk = new GameObject[10];
+    private GameObject[] OldChunk = new GameObject[10];
+    private int defaltPlanesScale = 8;
+    private float checkPointX = 0.0f;
+    private int planenum;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        for (int i = 0; i < chunkSize; i++) { ActiveChunk[i] = StartGround; }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlaneGenerate();
+        if (Maru.transform.position.x > checkPointX)
+        {
+            if (OldChunk[0] != null) { DeleteChunk(); }
+            for (int i = 0; i < chunkSize; i++) { OldChunk[i] = ActiveChunk[i]; }
+            ChunkGenerate();
+        }
+        
     }
 
-    void PlaneGenerate()
+    void ChunkGenerate()
     {
-        var MpointX = Maru.transform.position.x;
-        var parent = this.transform;
-        int nowplane = 0;
+        Debug.Log("新しいチャンクを生成します");
+        //右端にあるオブジェクトの右上端の座標を取得
+        float lastPointX = ActiveChunk[chunkSize - 1].transform.position.x + ActiveChunk[chunkSize - 1].transform.localScale.x / 2;
+        float lastPointY = ActiveChunk[chunkSize - 1].transform.position.y + ActiveChunk[chunkSize - 1].transform.localScale.y / 2;
 
-        if (posX - 10 < MpointX)
+        for (int i = 0; i < chunkSize; i++)
         {
-            lastplane = true;
-            postposX = posX;
-            for (int i = 0; i < step.Length; i++)
+            int planeScale = Random.Range(defaltPlanesScale/2, defaltPlanesScale*2+1); //床の幅を0.5倍～2倍までランダムに変える
+            int holeSize = Random.Range(2, 10); //穴の幅
+            int planeHeight = Random.Range(-2, 2); //ランダムに高さを変える
+
+            int selectplane = Random.Range(0, 100);
+            if (selectplane < 50)
             {
-                int selectplane = Random.Range(0, 100);
-                if (selectplane < 50)
-                {
-                    nowplane = 0;
-                }
-                else if (selectplane < 70)
-                {
-                    nowplane = 1;
-                }
-                else if (selectplane < 100)
-                {
-                    nowplane = 2;
-                }
-                poststep[i] = step[i];
-                posX += Random.Range(planesize + 3, planesize + 9);
-                posY += Random.Range(-3, 3);
-                if (posY > 7)
-                {
-                    posY = 7;
-                }
-                else if (posY < -3)
-                {
-                    posY = -3;
-                }
-                step[i] = Instantiate(Plane[nowplane], new Vector3(posX, posY, 0), Quaternion.identity, parent);
+                planenum = 0;
             }
-        }
-        if (postposX + 10 < MpointX && lastplane && poststep != null)
-        {
-            lastplane = false;
-            for (int i = 0; i < step.Length; i++)
+            else if (selectplane < 75)
             {
-                Destroy(poststep[i].gameObject);
+                planenum = 1;
             }
+            else if (selectplane < 100)
+            {
+                planenum = 2;
+            }
+
+            //右端のオブジェクトと新たに生成されるオブジェクトを
+            lastPointX += planeScale / 2 + holeSize;
+            lastPointY -= Planes[0].transform.localScale.y / 2 + planeHeight;
+            if (lastPointY < -10)
+            {
+                lastPointY += 2;
+            }else if(lastPointY > -1)
+            {
+                lastPointY -= 2;
+            }
+            Debug.Log(lastPointY);
+            ActiveChunk[i] = Instantiate(Planes[planenum], new Vector2(lastPointX, lastPointY), Quaternion.identity);
+            ActiveChunk[i].transform.localScale = new Vector2(planeScale, ActiveChunk[i].transform.localScale.y);
+
+            lastPointX = ActiveChunk[i].transform.position.x + ActiveChunk[i].transform.localScale.x / 2;
+            lastPointY = ActiveChunk[i].transform.position.y + ActiveChunk[i].transform.localScale.y / 2;
         }
+        
+        checkPointX = ActiveChunk[chunkSize / 2].transform.position.x;
+    }
+
+    void DeleteChunk()
+    {
+        Debug.Log("古いチャンクを消します");
+        for (int i = 0; i < chunkSize; i++) { Destroy(OldChunk[i].gameObject); }
     }
 }
